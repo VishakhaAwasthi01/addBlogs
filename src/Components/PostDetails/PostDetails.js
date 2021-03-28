616cd3f84ceb6118dedde4fc07438be2c374eb5f
vishakhaFincity <vishakha.awasthi@fincity.com>
2021-03-28 21:51:33 +0530

    fixed warning

--- src/Components/PostDetails/PostDetails.js
+++ src/Components/PostDetails/PostDetails.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import styles from '../PostList/PostList.module.css'
 import axios from 'axios'
 import moment from 'moment'
@@ -82,113 +82,118 @@ const PostDetails = () => {
     history.push(`/`)
   }
   return (
-    <div className={` ${parentDiv} mx-auto`}>
-      <div className="row">
-        <button class="btn" onClick={handleClick}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={20}
-            height={20}
-            fill="currentColor"
-            className="bi bi-arrow-left"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
-            />
-          </svg>
-          <span className="px-2">Back</span>
-        </button>
-      </div>
-      {showEdit && (
-        <AddBlog
-          setShowEdit={setShowEdit}
-          showEdit={showEdit}
-          postId={postId}
-        />
-      )}
+    <Fragment>
+      <ToastContainer />
+      <div className={` ${parentDiv} mx-auto`}>
+        <div className="row">
+          <button class="btn" onClick={handleClick}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={20}
+              height={20}
+              fill="currentColor"
+              className="bi bi-arrow-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+              />
+            </svg>
+            <span className="px-2">Back</span>
+          </button>
+        </div>
+        {showEdit && (
+          <AddBlog
+            setShowEdit={setShowEdit}
+            showEdit={showEdit}
+            postId={postId}
+          />
+        )}
 
-      <div className={`${postBox} my-4 `}>
-        <div className={`${dummyImg}`} style={{ overflow: 'hidden' }}>
-          <img src={getResponse?.imageUrl} alt="Img" />
-        </div>
-        <div className="m-4">
-          <h2>{getResponse?.title}</h2>
-          <p>
-            Created:
-            {moment(getResponse?.createdAt).format('MMMM Do YYYY, h:mm:ss a')}
-          </p>
-          <p>Last Updated: {getResponse?.updatedAt}</p>
-          <p>{getResponse?.description}</p>
-          <div className="d-flex">
-            <div className="">
-              <p className="">
-                <strong>{getComments?.count} comments</strong>
-              </p>
-            </div>
-            {getResponse?.userId === '73' && (
-              <div className="d-flex ml-auto">
-                <button
-                  className="btn px-4"
-                  onClick={() => {
-                    setShowEdit(true)
-                    window.scrollTo(0, 0)
-                  }}
-                >
-                  <strong> Edit</strong>
-                </button>
-                <button className="btn px-2">
-                  <strong> Delete</strong>
-                </button>
-              </div>
-            )}
+        <div className={`${postBox} my-4 `}>
+          <div className={`${dummyImg}`} style={{ overflow: 'hidden' }}>
+            <img src={getResponse?.imageUrl} alt="Img" />
           </div>
-        </div>
-      </div>
-      <div className="row mx-1">
-        <textarea
-          type="text"
-          name="comments"
-          className={`w-100 rounded ${inputBox} mt-3 px-2`}
-          style={{ minHeight: '100px' }}
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-        <button
-          className="btn px-4 ml-auto"
-          onClick={(e) => addComment(e, getResponse?.id)}
-        >
-          <strong> Comment</strong>
-        </button>
-      </div>
-      <div className="my-2">
-        {getComments?.items?.length ? (
-          getComments?.items?.map((each, i) => {
-            return (
-              <div key={i} className="my-4">
-                <p>{each?.commentBody}</p>
-                <div className="d-flex">
+          <div className="m-4">
+            <h2>{getResponse?.title}</h2>
+            <p>
+              Created:
+              {moment(getResponse?.createdAt).format('MMMM Do YYYY, h:mm:ss a')}
+            </p>
+            <p>Last Updated: {getResponse?.updatedAt}</p>
+            <p>{getResponse?.description}</p>
+            <div className="d-flex">
+              <div className="">
+                <p className="">
+                  <strong>{getComments?.count} comments</strong>
+                </p>
+              </div>
+              {getResponse?.userId === '73' && (
+                <div className="d-flex ml-auto">
                   <button
-                    onClick={(e) => deleteComment(e, each?.postId, each?.id)}
-                    className="btn pr-auto"
-                    style={{ textAlign: 'left', paddingLeft: '0px' }}
+                    className="btn px-4"
+                    onClick={() => {
+                      setShowEdit(true)
+                      window.scrollTo(0, 0)
+                    }}
                   >
+                    <strong> Edit</strong>
+                  </button>
+                  <button className="btn px-2">
                     <strong> Delete</strong>
                   </button>
-                  <p className="ml-auto" style={{ fontWeight: '500' }}>
-                    Created:
-                    {moment(each?.createdAt).format('MMMM Do YYYY, h:mm:ss a')}
-                  </p>
                 </div>
-              </div>
-            )
-          })
-        ) : (
-          <p className="text-info">No comments</p>
-        )}
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="row mx-1">
+          <textarea
+            type="text"
+            name="comments"
+            className={`w-100 rounded ${inputBox} mt-3 px-2`}
+            style={{ minHeight: '100px' }}
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <button
+            className="btn px-4 ml-auto"
+            onClick={(e) => addComment(e, getResponse?.id)}
+          >
+            <strong> Comment</strong>
+          </button>
+        </div>
+        <div className="my-2">
+          {getComments?.items?.length ? (
+            getComments?.items?.map((each, i) => {
+              return (
+                <div key={i} className="my-4">
+                  <p>{each?.commentBody}</p>
+                  <div className="d-flex">
+                    <button
+                      onClick={(e) => deleteComment(e, each?.postId, each?.id)}
+                      className="btn pr-auto"
+                      style={{ textAlign: 'left', paddingLeft: '0px' }}
+                    >
+                      <strong> Delete</strong>
+                    </button>
+                    <p className="ml-auto" style={{ fontWeight: '500' }}>
+                      Created:
+                      {moment(each?.createdAt).format(
+                        'MMMM Do YYYY, h:mm:ss a',
+                      )}
+                    </p>
+                  </div>
+                </div>
+              )
+            })
+          ) : (
+            <p className="text-info">No comments</p>
+          )}
+        </div>
       </div>
-    </div>
+    </Fragment>
   )
 }
 export default PostDetails
